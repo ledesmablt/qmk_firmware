cd9ddd50f407aa8f21a4185cda15c81ead7a1f51
Benj Ledesma <benj.ledesma@gmail.com>
2020-10-22 15:42:53 +0800

    vim light on activate; changed key to Lwin; toggle layers with FN+num

--- keyboards/kbdfans/kbd75/keymaps/ledesmablt/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/ledesmablt/keymap.c
@@ -13,10 +13,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   KC_MPLY, 
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   KC_BSPC,  KC_HOME, 
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP, 
-    LT(_VIM, KC_CAPS),
-              KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN, 
+    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN, 
     KC_LSFT,  MO(_FN1), KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END, 
-    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   KC_SPC,   KC_SPC,                       MO(_FN1), KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LCTL,  LT(_VIM, KC_LGUI),  
+                        KC_LALT,                      KC_SPC,   KC_SPC,   KC_SPC,                       MO(_FN1), KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [_FN1] = LAYOUT(
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                                                                                                                   DYN_REC_START1,  DYN_REC_START2,
                                                                                                                                       KC_TRNS,  KC_TRNS,  KC_TRNS, 
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_TRNS,  RESET,    KC_TRNS, 
+    KC_TRNS,  TO(0),    TO(1),    TO(2),    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_TRNS,  RESET,    KC_TRNS, 
     KC_TRNS,  RGB_TOG,  RGB_MOD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_SAI,  KC_TRNS,  KC_TRNS,  RGB_HUD,  BL_DEC,   BL_INC,   KC_TRNS,            KC_TRNS, 
     KC_TRNS,  RGB_M_P,  RGB_M_B,  RGB_SAD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_VAD,  RGB_VAI,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_HUI,  KC_TRNS,  KC_MPRV,  KC_MNXT,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS, 
@@ -62,6 +62,26 @@ uint16_t RGB_current_mode;
 uint16_t RGB_current_hue;
 uint16_t RGB_current_sat;
 uint16_t RGB_current_val;
+
+// customs
+void enable_vim_light(void) {
+    rgblight_sethsv(HSV_TEAL);
+}
+
+
+void enable_macro_light(void) {
+    rgblight_sethsv(HSV_SPRINGGREEN);
+    rgblight_mode(5);
+}
+
+void enable_caps_light(void) {
+    if (!rgblight_config.enable) {
+        rgblight_enable();
+    }
+    rgblight_sethsv(HSV_GOLDENROD);
+    rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
+}
+
 
 // Setup RGB
 void keyboard_post_init_user(void) {
@@ -137,21 +157,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 
-void enable_macro_light(void) {
-    rgblight_sethsv(HSV_SPRINGGREEN);
-    rgblight_mode(5);
-}
-
-void enable_caps_light(void) {
-    if (!rgblight_config.enable) {
-        rgblight_enable();
-    }
-    rgblight_sethsv(HSV_GOLDENROD);
-    rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
-}
-
 bool led_update_user(led_t led_state) {
-    if (led_state.caps_lock) {
+    if (layer_state_is(_VIM)) {
+        enable_vim_light();
+    } else if (led_state.caps_lock) {
         enable_caps_light();
     } else if (isRecording) {
         enable_macro_light();
