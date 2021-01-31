e617291fe6f34bfe955c9a50b48c6d5814df5237
Benj Ledesma <benj.ledesma@gmail.com>
2021-01-31 20:32:15 +0800

    Relayout, remove lower2

--- keyboards/planck/keymaps/ledesmablt/keymap.c
+++ keyboards/planck/keymaps/ledesmablt/keymap.c
@@ -21,7 +21,6 @@
 enum planck_layers {
   _QWERTY,
   _LOWER,
-  _LOWER2,
   _RAISE,
   _RAISE2,
   _PLOVER,
@@ -35,7 +34,6 @@ enum planck_keycodes {
 };
 
 #define LOWER MO(_LOWER)
-#define LOWER2 MO(_LOWER2)
 #define RAISE MO(_RAISE)
 #define RAISE2 MO(_RAISE2)
 
@@ -45,34 +43,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LCTL, KC_LGUI, KC_LALT, LOWER2,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   RAISE2,  KC_RCTRL,KC_RSFT, KC_RSFT
+    KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   RAISE2,  KC_RCTRL,KC_RSFT, KC_RSFT
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-    _______, _______, _______, KC_HOME, KC_END,  _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-),
-[_LOWER2] = LAYOUT_planck_grid(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-    _______, KC_F11,  KC_F12,  _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    _______, _______, KC_LCBR, KC_LBRC, KC_LPRN, _______, _______, KC_RPRN, KC_RBRC, KC_RCBR, KC_PIPE, LSFT(KC_QUOT),
+    _______, _______, _______, KC_HOME, KC_END,  _______, _______, KC_PGDN, KC_PGUP, LSFT(KC_COMM),LSFT(KC_DOT),_______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_BSLS, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______, _______, KC_MPLY,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
-
 [_RAISE2] = LAYOUT_planck_grid(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_DEL,
-    _______, KC_LCBR, KC_LBRC, KC_LPRN, _______, _______, _______, _______, KC_RPRN, KC_RBRC, KC_RCBR, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+    _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
